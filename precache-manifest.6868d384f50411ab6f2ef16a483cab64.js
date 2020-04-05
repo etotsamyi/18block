37e81d5967c1757d90fb001dd302d5657375d327
etotsamyi <51693784+etotsamyi@users.noreply.github.com>
2020-04-05 12:21:28 +0300

    Updates

---
+++ precache-manifest.6868d384f50411ab6f2ef16a483cab64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc669eb869a9349169e88ec3695a907f",
+    "url": "/18block/index.html"
+  },
+  {
+    "revision": "a1bd397e5a532793f335",
+    "url": "/18block/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "afa42b9bc9e02285573a",
+    "url": "/18block/static/css/main.d8e5ada6.chunk.css"
+  },
+  {
+    "revision": "a1bd397e5a532793f335",
+    "url": "/18block/static/js/2.6efb24ef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/18block/static/js/2.6efb24ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa42b9bc9e02285573a",
+    "url": "/18block/static/js/main.60f8c2b6.chunk.js"
+  },
+  {
+    "revision": "cd7a84744c373df08c7f",
+    "url": "/18block/static/js/runtime-main.632cba27.js"
+  },
+  {
+    "revision": "a61426fa482f6ba902c3ee7cfc4e4247",
+    "url": "/18block/static/media/hru.a61426fa.mp3"
+  }
+]);
