ba31d195f11d51d70c52df5913b3c3d275e30077
etotsamyi <51693784+etotsamyi@users.noreply.github.com>
2020-04-09 14:12:21 +0300

    не работает

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -3,43 +3,44 @@ import { Slider, Progress, Button } from 'antd';
 import music from '../assets/media/timer-end.mp3';
 
 export default class Countdown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = ({
-      minutes: 0,
-      seconds: 0,
-      active: false,
-      allTime: 0,
-    });
+  state = {
+    seconds: 0,
+    active: false,
+    allTime: 0,
+  }
+
+  secondsToBeautifulTime = () => {
+    const { seconds } = this.state;
+    const minute = Math.floor(seconds / 60);
+    return { minute, second: seconds - minute * 60 };
   }
 
   handleSliderChange = (e) => {
     this.setState({
-      seconds: e % 60,
-      minutes: e >= 60 ? Math.floor(e / 60) : 0,
+      seconds: e,
     });
   }
 
   handleSecondsChange = () => (e) => {
-    this.setState({ seconds: e.target.value });
+    const { seconds } = this.state;
+    const prevTime = seconds;
+    this.setState({ seconds: prevTime + Number(e.target.value) });
   }
 
   handleMinutesChange = () => (e) => {
-    this.setState({ minutes: e.target.value });
+    const { seconds } = this.state;
+    const prevTime = seconds;
+    this.setState({ seconds: prevTime + Number(e.target.value) * 60 });
   }
 
   run = () => {
     const {
-      seconds, minutes, active,
+      seconds, active,
     } = this.state;
-    if (minutes === 0 && seconds === 1 && active) {
+    if (seconds === 0) {
       clearInterval(this.timerID);
       this.setState({ seconds: seconds - 1, active: !active });
       this.soundPlay();
-      return;
-    }
-    if (seconds === 0) {
-      this.setState({ seconds: 59, minutes: minutes - 1 });
     } else {
       this.setState({ seconds: seconds - 1 });
     }
@@ -47,12 +48,12 @@ export default class Countdown extends React.Component {
 
   handleStartStop = () => {
     const {
-      seconds, minutes, active, allTime,
+      seconds, active, allTime,
     } = this.state;
     if (allTime === 0) {
       this.setState({
         active: !active,
-        allTime: Number(minutes) * 60 + Number(seconds),
+        allTime: Number(seconds),
       });
     } else {
       this.setState({ active: !active });
@@ -69,7 +70,6 @@ export default class Countdown extends React.Component {
     this.setState({
       active: 0,
       seconds: 0,
-      minutes: 0,
       allTime: 0,
     });
   }
@@ -81,17 +81,18 @@ export default class Countdown extends React.Component {
 
   render() {
     const {
-      minutes, seconds, active, allTime,
+      seconds, active, allTime,
     } = this.state;
-    const currentValue = (Number(minutes) * 60 + Number(seconds)) / allTime;
+    const currentValue = (Number(seconds)) / allTime;
     const progress = Math.round(100 - currentValue * 100);
+    const { minute, second } = this.secondsToBeautifulTime();
 
     return (
       <div>
         <Slider
           max={3600}
           min={0}
-          value={Number(minutes) * 60 + Number(seconds)}
+          value={Number(seconds)}
           onChange={(e) => this.handleSliderChange(e)}
           disabled={active}
           step={15}
@@ -100,7 +101,7 @@ export default class Countdown extends React.Component {
           type="number"
           min="0"
           max="720"
-          value={minutes}
+          value={minute}
           onChange={this.handleMinutesChange()}
           className="btn"
           placeholder="минуты"
@@ -110,7 +111,7 @@ export default class Countdown extends React.Component {
           type="number"
           min="0"
           max="59"
-          value={seconds}
+          value={second}
           onChange={this.handleSecondsChange()}
           className="btn"
           placeholder="секунды"
@@ -119,7 +120,7 @@ export default class Countdown extends React.Component {
         <Button
           className="btn"
           onClick={this.handleStartStop}
-          disabled={minutes === 0 && seconds === 0}
+          disabled={seconds === 0}
         >
           {active ? 'Пауза' : 'Старт'}
         </Button>
@@ -131,10 +132,10 @@ export default class Countdown extends React.Component {
           Сброс
         </Button>
         <div className="clock">
-          {minutes < 10 ? ` 0${minutes}` : ` ${minutes}`}
+          {minute}
           {' '}
           :
-          {seconds < 10 ? ` 0${seconds}` : ` ${seconds}`}
+          {second}
         </div>
         <Progress
           type="circle"
